f3eef3a9d3ace1038caa74cdcd620de83c5d7e6d
Pedro Coelho Torres <pedro.torres@intelerad.com>
2020-11-10 21:05:02 -0500

    - use router links on not found component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialCommonsModule } from '@my-fintech/commons/material-commons.module';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NotFoundComponent],
   imports: [
-    CommonModule, MaterialCommonsModule
+    CommonModule, RouterModule, MaterialCommonsModule
   ]
 })
 export class SharedModule { }
